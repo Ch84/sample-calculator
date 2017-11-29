69016b6576b15c724ac0d85b9854e218d9f8f6e4
Christian Hall <christian_hall@hotmail.com>
2017-11-29 20:46:52 +0100

    Thirteenth commit. Tested the methods testMultiplyMediumNegativeNumbers and testMultiplyLargeNegativeNumbers. Both the methods passed the coverage test.

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -356,8 +356,42 @@ public class CalculatorTest {
 						}
 						
 					}
-				
+					@Test
+					public void testMultiplyMediumNegativeNumbers() {
+						double firstNumber = 0;
+						double secondNumber = 0;
+						double result = 0;
+						
+						for (int k=0; k<200; k++) {
+							firstNumber = Double.valueOf(df.format(random.nextDouble()*-100));
+							secondNumber = Double.valueOf(df.format(random.nextDouble()*-100));
+							result = firstNumber * secondNumber;
+							
+							LOG.info("Testing the method multiply with: " + firstNumber + " and " + secondNumber);
+							assertEquals(Math.round(calculator.multiply(firstNumber, secondNumber)), Math.round(result), 1);
+							
+						}
+						
+					}
+					@Test
+					public void testMultiplyLargeNegativeNumbers() {
+						double firstNumber = 0;
+						double secondNumber = 0;
+						double result = 0;
+						
+						for (int k=0; k<200; k++) {
+							firstNumber = Double.valueOf(df.format(random.nextDouble()*-1000));
+							secondNumber = Double.valueOf(df.format(random.nextDouble()*-1000));
+							result = firstNumber * secondNumber;
+							
+							LOG.info("Testing the method multiply with: " + firstNumber + " and " + secondNumber);
+							assertEquals(Math.round(calculator.multiply(firstNumber, secondNumber)), Math.round(result), 1);
+							
+						}
+					}
 				}
+				
+				
 				
 			
 		
